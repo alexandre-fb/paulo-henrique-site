d36de60e5ac010b2b1ec2cedb5179e47b62f8221
alexandrefb <alexandrefb.86@gmail.com>
2023-09-02 13:48:31 -0300

    :tada: Commit inicial

---
+++ components/SectionTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2 class="relative w-min text-text-color font-title text-4xl"
+    >
+        <span>{{ title }}</span>
+        <span v-if="showLine" class="block absolute w-14 h-[2px] bg-primary -right-20 bottom-4"></span>
+    </h2>
+</template>
+  
+<script>
+export default {
+    props: {
+        title: String,
+        showLine: Boolean
+    },
+    data() {
+        return {
+        }
+
+    }
+};
+</script>
+  
+<style scoped>
+</style>
