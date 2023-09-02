f56c1127f8f80d3dc02e66acf0f202829a70777d
alexandrefb <alexandrefb.86@gmail.com>
2023-09-02 10:41:33 -0300

    :sparkles: feat: Add componentes

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-   <Header />
+    <Header />
+    <Banner />
+    <Sobre />
   </div>
 </template>
+
+<script>
+import Banner from './components/sections/Banner.vue';
+import Sobre from './components/sections/Sobre.vue';
+
+export default {
+  components: {
+    Banner,
+    Sobre
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
