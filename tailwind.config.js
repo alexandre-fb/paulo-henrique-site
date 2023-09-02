5f457011665d655cb2eab853963dbd1e70554029
alexandrefb <alexandrefb.86@gmail.com>
2023-09-02 10:42:42 -0300

    :sparkles: feat: Add Configurações

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
+    colors: {
+      'primary': '#506F7F',
+      'secondary': '#212932',
+      'secondary-75': 'rgba(33, 41, 50, .75)',
+      'tertiary': '#ff49db',
+      'quaternary': '#ff7849',
+      'text-color': '#fff',
+      'white': '#fff',
+      'black': '#000',
+    },
+    fontFamily: {
+      text: ['Open Sans', 'sans-serif'],
+      title: ['Nunito', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
