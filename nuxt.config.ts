e1f594ebbba3954324ac7f70d201b6b6e83fe12a
alexandrefb <alexandrefb.86@gmail.com>
2023-09-02 10:41:57 -0300

    :sparkles: feat: Add font families

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-fonts', 
+      {
+        families: {
+          Nunito: {
+            wght: [300, 400, 600, 700], 
+          },
+          OpenSans: {
+            wght: [300, 400, 600, 700], 
+          }
+        }
+      }
+    ]
+  ],
+  css: ['~/assets/css/main.css']
+
 })
