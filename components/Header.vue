dd10a4fac27c619dc57760736d36e6abe90a3c06
alexandrefb <alexandrefb.86@gmail.com>
2023-09-02 10:43:04 -0300

    :tada: Commit inicial

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,179 @@
 <template>
-    <h1 class="text-green-500">HEADER</h1>
+  <div
+    class="flex justify-center w-full px-3 h-[60px] fixed top-0 z-50"
+    :class="scrolled ? 'header__container' : 'header__container page-top'"
+  >
+    <nav class="relative flex items-center justify-center w-full max-w-7xl">
+      <!-- LOGO -->
+      <div class="absolute left-0 text-text-color">
+        <h3 class="text-xl mb-0 font-title">Paulo Henrique</h3>
+        <span class="text-xs font-text">Móveis e Arquitetura</span>
+      </div>
+
+      <!-- MENU DESKTOP -->
+      <ul class="items-center gap-11 hidden lg:flex">
+        <li class="relative" v-for="item in menuItems" :key="item.value">
+          <a href="" class="menu-desktop__link text-text-color">{{
+            item.label
+          }}</a>
+        </li>
+      </ul>
+
+      <!-- MENU MOBILE -->
+      <div class="absolute right-0 lg:hidden">
+        <!-- HAMBURGER -->
+        <div
+          class="menu__hamburger space-y-2 cursor-pointer"
+          :class="menuMobileIsOpen ? 'hamburgerIsActive' : ''"
+          @click="toogleMenu"
+        >
+          <div class="w-8 h-0.5 bg-white"></div>
+          <div class="w-8 h-0.5 bg-white"></div>
+          <div class="w-8 h-0.5 bg-white"></div>
+        </div>
+
+        <!-- LINKS MOBILE -->
+        <div
+          class="absolute z-50 bg-secondary w-60 right-0 top-10 p-5 text-right transition-all duration-200 ease-in-out"
+          :class="menuMobileIsOpen ? 'linksMobileVisible' : 'linksMobileHidden'"
+        >
+          <ul class="flex-col items-center gap-11">
+            <li class="relative" v-for="item in menuItems" :key="item.value">
+              <a href="" class="menu-desktop__link text-text-color">{{
+                item.label
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrolled: false,
+      menuItems: [
+        {
+          label: "Início",
+          value: "inicio",
+          link: "inicio",
+        },
+        {
+          label: "Sobre",
+          value: "sobre",
+          link: "sobre",
+        },
+        {
+          label: "Projetos",
+          value: "projetos",
+          link: "projetos",
+        },
+        {
+          label: "Skills",
+          value: "skills",
+          link: "skills",
+        },
+        {
+          label: "Contato",
+          value: "contato",
+          link: "contato",
+        },
+      ],
+      menuMobileIsOpen: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.scrolled = window.scrollY > 0;
+    },
+    toogleMenu() {
+      this.menuMobileIsOpen = !this.menuMobileIsOpen;
+    },
+    openMenuMobile() {
+      this.menuMobileIsOpen = true;
+    },
+    closeMenuMobile() {
+      this.menuMobileIsOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header__container {
+  background-color: rgba(33, 41, 50, .75);
+  transition: background-color 200ms ease-in-out; 
+}
+
+.header__container.page-top {
+  background-color: transparent;
+}
+
+
+
+/*HOVER LINKS*/ 
+.menu-desktop__link {
+  display: block;
+  padding: 8px 0px;
+  position: relative;
+}
+
+.menu-desktop__link:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 1px solid transparent;
+  height: 0;
+  width: 0;
+}
+
+.menu-desktop__link:hover:after {
+  content: "";
+  border-bottom: 1px solid white;
+  border-left: none;
+  border-top: none;
+  height: 100%;
+  width: 100%;
+  transition: width 200ms ease-in-out;
+}
+
+/*MENU HAMBURGER*/
+.menu__hamburger > div {
+  transition: 200ms ease-in-out;
+}
+
+.menu__hamburger.hamburgerIsActive :first-child {
+  transform: rotate(-225deg);
+  position: relative;
+  top: 10px;
+}
+
+.menu__hamburger.hamburgerIsActive :nth-child(2) {
+  opacity: 0;
+}
+
+.menu__hamburger.hamburgerIsActive :last-child {
+  transform: rotate(45deg);
+  position: relative;
+  top: -10px;
+}
+
+.linksMobileHidden {
+  transform: translateX(200px);
+  opacity: 0;
+}
+.linksMobileVisible {
+  transform: translateX(16px);
+  opacity: 1;
+}
+</style>
---
+++ components/sections/Sobre.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="banner__container relative flex justify-center w-full px-3 h-full bg font-text bg-primary"
+  >
+    <picture class="absolute w-full h-[100%]">
+      <source
+        media="(max-width: 769px)"
+        srcset="~/assets/images/banner-bg-mobile.webp"
+      />
+      <img
+        src="~/assets/images/banner-bg-desktop.webp"
+        alt="Textura de mandeira"
+        class="w-full h-[100%] object-cover mix-blend-multiply"
+      />
+    </picture>
+    <div class="relative flex items-center w-full max-w-7xl">
+      <div
+        class="relative -top-[5%] left-1 md:left-[7%] px-3 flex flex-col items-start lg:items-end"
+      >
+        <h2
+          class="text-text-color text-5xl lg:text-6xl font-title font-light mb-5"
+        >
+          Paulo Henrique
+        </h2>
+        <h3 class="text-text-color text-2xl lg:text-3xl font-text font-light mb-4">
+          Móveis e Arquitetura
+        </h3>
+        <span class="absolute block h-[2px] w-[30%] bg-primary -bottom-1"></span>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+  
+<style scoped>
+.banner__container {
+  height: calc(100vh - 60px);
+}
+</style>
