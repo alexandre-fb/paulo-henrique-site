8fd6ec97aadb750710107cae97896c251a2f1bde
alexandrefb <alexandrefb.86@gmail.com>
2023-09-08 00:29:58 -0300

    :sparkles: Criado arquivo

---
+++ components/icons/IconCheck.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg :width="width" :height="height" viewBox="0 0 10 7" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M9.791 0.205046C10.07 0.478441 10.07 0.922434 9.791 1.19583L4.07736 6.79495C3.79837 7.06835 3.34529 7.06835 3.06631 6.79495L0.209484 3.99539C-0.0695025 3.722 -0.0695025 3.278 0.209484 3.00461C0.488471 2.73121 0.941545 2.73121 1.22053 3.00461L3.57295 5.30769L8.78219 0.205046C9.06118 -0.0683487 9.51425 -0.0683487 9.79324 0.205046H9.791Z"
+            :fill="color" />
+    </svg>
+</template>
+    
+<script>
+export default {
+    props: {
+        color: {
+            type: String,
+            default: '#506F7F'
+        },
+        width: String,
+        height: String
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+    
+<style scoped></style>
---
+++ components/sections/Skills.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="relative flex justify-center items-center w-full min-h-screen px-3 py-20 h-full font-text bg-secondary">
+    <div class="gap-7 w-full max-w-7xl">
+
+      <div class="mb-16">
+        <SectionTitle title="Skills" showLine />
+      </div>
+
+      <div class="flex flex-col relative justify-between lg:flex-row space-y-10">
+
+        <!-- HARDSKILLS -->
+        <div class="border border-primary p-5 lg:w-[51.5%] lg:p-20">
+          <h3 class="text-text-color text-2xl mb-4">Hard Skills</h3>
+          <ul class="space-y-4">
+            <li v-for="(hardSkill, index) in hardSkills" :key="hardSkill + index" class="flex">
+              <IconCheck width="12px" color="#fff" />
+              <span class="ml-3 text-text-color font-text font-light text-lg transition hover:scale-[1.01] cursor-default">{{ hardSkill }}</span>
+            </li>
+          </ul>
+        </div>
+  
+        <!-- SOFTSKILLS -->
+        <div class="right-0 top-20 border border-primary p-5 lg:absolute lg:w-[51.5%] lg:p-20">
+          <h3 class="text-text-color text-2xl mb-4">Soft Skills</h3>
+          <ul class="space-y-4">
+            <li v-for="(softSkill, index) in softSkills" :key="softSkill + index" class="flex">
+              <IconCheck width="12px" color="#fff" />
+              <span class="ml-3 text-text-color font-text font-light text-lg transition hover:scale-[1.01] cursor-default">{{ softSkill }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+  
+<script>
+import IconCheck from '../icons/IconCheck.vue';
+import SectionTitle from '../SectionTitle.vue';
+
+
+export default {
+  components: {
+    SectionTitle,
+    IconCheck
+  },
+  data() {
+    return {
+      hardSkills: [
+        'Modelagem 3D no SketchUp',
+        'Desenho Técnico',
+        'Cálculos de Dimensionamento',
+        'Conhecimento de Materiais e Construção',
+        'Desenvolvimento de Mobiliário'
+      ],
+      softSkills: [
+        'Trabalho em Equipe',
+        'Criatividade e Inovação',
+        'Resolução de Problemas',
+        'Adaptabilidade',
+        'Pensamento Analítico'
+      ],
+    }
+  },
+  methods: {
+
+  },
+};
+</script>
+  
+<style scoped></style>
